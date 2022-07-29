97c79556d230fcfdaa6bbec26a1f1391048424cb
Jason-Yxj <1049203433@qq.com>
2022-07-29 18:18:46 +0800

    修改外参提取代码，之前外参list放在循环外了。

--- npbgplusplus/data/export_data.py
+++ npbgplusplus/data/export_data.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.metrics import jaccard_score
 import yaml
 import os
 import open3d as o3d
@@ -13,7 +14,6 @@ def main(state, cfg, base_dir, save_dir, frame_skip):
     base_dir = os.path.join(base_dir, f'scans_{state}')
     save_dir = os.path.join(save_dir, f'scans_{state}')
 
-    extrinsics = []
     for scene_cfg in cfg_train:
         scene_name = scene_cfg['dataset_name']
         sens_path = os.path.join(base_dir, scene_name, f'{scene_name}.sens')
@@ -21,15 +21,16 @@ def main(state, cfg, base_dir, save_dir, frame_skip):
         os.makedirs(save_scene_dir, exist_ok = True)
 
         sd = SensorData(sens_path)
-        W, H = sd.color_width, sd.color_height
-        K = sd.intrinsic_color
-        intrinsics_path = os.path.join(save_scene_dir, 'intrinsics.npy')
-        print('exporting camera intrinsics to', intrinsics_path)
-        intrinsics = {'width': W, 'height': H, 'K': K[:3, :3]}
-        np.save(intrinsics_path, intrinsics)
+        # W, H = sd.color_width, sd.color_height
+        # K = sd.intrinsic_color
+        # intrinsics_path = os.path.join(save_scene_dir, 'intrinsics.npy')
+        # print('exporting camera intrinsics to', intrinsics_path)
+        # intrinsics = {'width': W, 'height': H, 'K': K[:3, :3]}
+        # np.save(intrinsics_path, intrinsics)
         
-        sd.export_color_images(os.path.join(save_scene_dir, 'images'), frame_skip = frame_skip)
+        # sd.export_color_images(os.path.join(save_scene_dir, 'images'), frame_skip = frame_skip)
         
+        extrinsics = []
         extrinsics_path = os.path.join(save_scene_dir, 'extrinsics.npy')
         print('exporting', len(sd.frames)//frame_skip, 'camera poses to', extrinsics_path)
         for f in tqdm(range(0, len(sd.frames), frame_skip), desc='export pose'):
@@ -55,5 +56,5 @@ config_path = '/cwang/home/yxj/Project/npbgpp/configs/datasets/scannet_pretrain.
 f = open(config_path)
 cfg = yaml.load(f.read(), Loader=yaml.FullLoader)
 
-# main('train', cfg, base_dir, save_dir, frame_skip = 20)
-cpy_ply(save_dir)
+main('train', cfg, base_dir, save_dir, frame_skip = 20)
+# cpy_ply(save_dir)
